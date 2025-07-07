18c865d3086994310d23664c3e21f9eb6ee75b20
glebgutnik <glebgytnik@gmail.com>
2025-07-07 23:09:44 +0300

    Initialize site layout

--- site/src/jsMain/kotlin/io/skobinsky/septwedding/AppEntry.kt
+++ site/src/jsMain/kotlin/io/skobinsky/septwedding/AppEntry.kt
@@ -1,22 +1,21 @@
 package io.skobinsky.septwedding
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import com.varabyte.kobweb.compose.KobwebComposeStyles
 import com.varabyte.kobweb.compose.css.ScrollBehavior
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxHeight
 import com.varabyte.kobweb.compose.ui.modifiers.scrollBehavior
 import com.varabyte.kobweb.core.App
+import com.varabyte.kobweb.core.KobwebApp
+import com.varabyte.kobweb.silk.ColorModeAware
 import com.varabyte.kobweb.silk.SilkApp
-import com.varabyte.kobweb.silk.components.layout.Surface
+import com.varabyte.kobweb.silk.SilkFoundationStyles
 import com.varabyte.kobweb.silk.init.InitSilk
 import com.varabyte.kobweb.silk.init.InitSilkContext
 import com.varabyte.kobweb.silk.init.registerStyleBase
-import com.varabyte.kobweb.silk.style.common.SmoothColorStyle
-import com.varabyte.kobweb.silk.style.toModifier
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
 import com.varabyte.kobweb.silk.theme.colors.loadFromLocalStorage
-import com.varabyte.kobweb.silk.theme.colors.saveToLocalStorage
 import com.varabyte.kobweb.silk.theme.colors.systemPreference
 
 private const val COLOR_MODE_KEY = "septwedding:colorMode"
@@ -37,14 +36,10 @@ fun initStyles(ctx: InitSilkContext) {
 @App
 @Composable
 fun AppEntry(content: @Composable () -> Unit) {
-    SilkApp {
-        val colorMode = ColorMode.current
-        LaunchedEffect(colorMode) {
-            colorMode.saveToLocalStorage(COLOR_MODE_KEY)
-        }
-
-        Surface(SmoothColorStyle.toModifier().fillMaxHeight()) {
-            content()
-        }
+    KobwebApp {
+        KobwebComposeStyles()
+        SilkFoundationStyles()
+        //ColorModeAware()
+        content()
     }
 }
---
+++ site/src/jsMain/kotlin/io/skobinsky/septwedding/theme/AppColors.kt
@@ -0,0 +1,23 @@
+package io.skobinsky.septwedding.theme
+
+import org.jetbrains.compose.web.css.Color
+
+object AppColors {
+    val BrandPink = Color("#f5dac5")
+    val BrandBeige = Color("#cc9d8b")
+    val BrandBrown = Color("#6c5450")
+    val BrandBrownWarm = Color("#9c5e49")
+    val BrandGreen = Color("#c1c4a9")
+    val BrandGreenDark = Color("#546c52")
+    val BrandBlack = Color("#000000")
+
+    val BrandColors = listOf(
+        BrandPink,
+        BrandBeige,
+        BrandBrown,
+        BrandBrownWarm,
+        BrandGreen,
+        BrandGreenDark,
+        BrandBlack
+    )
+}
